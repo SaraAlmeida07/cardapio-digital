83be4c0d95dcc2617a90bfa5185335010bdd00e4
SaraAlmeida07 <saraabner07@gmail.com>
2026-04-07 13:53:04 -0300

    chore: setup inicial do projeto Vue 3 e limpeza de boilerplate

---
+++ cardapio/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, computed } from 'vue'
+// Aqui ficará o array de produtos e a lógica principal
+</script>
+
+<template>
+  <div>
+    <h1>Gerenciador de Cardápio</h1>
+    </div>
+</template>
+
+<style scoped>
+/* estilos globais simples virão aqui */
+</style>
